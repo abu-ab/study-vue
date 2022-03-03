f22c9380f6a77daeb97a1b8e6548fa6053d08227
gray <1012518214@qq.com>
2022-03-03 17:11:12 +0800

    feat: add stop & onStop

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,12 +1,33 @@
+import { extend } from "../shared";
+
+let activeEffect
 class ReactiveEffect {
     private _fn: any;
+    deps = [];
+    active = true;
+    onStop?: () => void;
     constructor(fn, public scheduler?) {
-        this._fn = fn;
+        this._fn = fn; ``
     }
     run() {
         activeEffect = this;
         return this._fn();
     }
+    stop() {
+        if (this.active) {
+            cleanupEffect(this);
+            if (this.onStop) {
+                this.onStop();
+            }
+            this.active = false
+        }
+    }
+}
+
+function cleanupEffect(effect) {
+    effect.deps.forEach((dep: any) => {
+        dep.delete(effect);
+    })
 }
 
 const targetMap = new Map();
@@ -23,7 +44,10 @@ export function track(target, key) {
         dep = new Set();
         depsMap.set(key, dep);
     }
+
+    if (!activeEffect) return;
     dep.add(activeEffect);
+    activeEffect.deps.push(dep);
 }
 
 export function trigger(target, key) {
@@ -39,12 +63,21 @@ export function trigger(target, key) {
     }
 }
 
-let activeEffect
 export function effect(fn, options: any = {}) {
     // fn
+
     const scheduler = options.scheduler;
     const _effect = new ReactiveEffect(fn, scheduler);
+    // options
+    // Object.assign(_effect,options)
+    //extend
+    extend(_effect, options);
     _effect.run()
+    const runner: any = _effect.run.bind(_effect);
+    runner.effect = _effect;
+    return runner;
+}
 
-    return _effect.run.bind(_effect);
+export function stop(runner) {
+    runner.effect.stop();
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -1,5 +1,5 @@
 import { reactive } from "../reactive"
-import { effect } from "../effect"
+import { effect, stop } from "../effect"
 describe("effect", () => {
     it("happy path", () => {
         const user = reactive({
@@ -54,4 +54,36 @@ describe("effect", () => {
         // should have run 
         expect(dummy).toBe(2);
     });
+
+    it('stop', () => {
+        let dummy;
+        const obj = reactive({ props: 1 });
+        const runner = effect(() => {
+            dummy = obj.props;
+        })
+        obj.props = 2;
+        expect(dummy).toBe(2);
+        stop(runner);
+        obj.props = 3;
+        expect(dummy).toBe(2);
+
+        runner();
+        expect(dummy).toBe(3);
+    });
+
+    it("onStop", () => {
+        const obj = reactive({
+            foo: 1
+        })
+        const onStop = jest.fn();
+        let dummy;
+        const runner = effect(() => {
+            dummy = obj.foo;
+        }, {
+            onStop
+        });
+        stop(runner);
+        expect(onStop).toBeCalledTimes(1);
+
+    })
 })
